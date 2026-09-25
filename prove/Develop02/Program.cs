a4562ed381a4ec3a0b437ce2bb48a32dd66c941b
Jackson Spencer <jacksonspencer327@gmail.com>
2026-09-25 11:11:58 -0700

    Commit 1

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
-    }
+class Globals {
+  public static List<string> _prompts = new List <string> {
+    "How many cats did you see today",
+    "Why are you the way that you are?",
+    "What is the meaning of life, the universe, and everything?",
+    "What is the natural logarithm of the integral of 8x^2 sin x from 1 to 2?",
+    "Who made you fill out this list?",
+    "When in unix time did you execute this program?",
+    "How many nuns could a nunchuck chuck if a nunchuck could chuck nuns?",
+    "Should I be scared of you?",
+    "Do two wrongs make a right?",
+    "In this world, is it kill or be killed?",
+    "Where is the worst place to try to eat a pizza?",
+  };    
+    
+    public static Random _rng = new Random();
 }
